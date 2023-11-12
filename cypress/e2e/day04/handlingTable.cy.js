f66f4703ef1ecf6494c5ac105674179c4bece60b
nuri Kara <nurikara@yahoo.com>
2023-11-12 22:13:47 +0300

    day10

--- cypress/e2e/day04/handlingTable.cy.js
+++ cypress/e2e/day04/handlingTable.cy.js
@@ -1,23 +1,18 @@
 describe('HandleTables', () => {
 
-//     beforeEach(() => {
-// cy.visit('https://demo.opencart.com/admin/index.php')
+    beforeEach(() => {
+cy.visit('https://demo.opencart.com/admin/index.php?route=common/dashboard ')
 
+cy.get('#input-username').type("demo")
+cy.get('#input-password').type("demo")
+cy.get('[class="btn btn-primary"]').click()
 
-
-
-// cy.get('#input-username').type("demo")
-// cy.get('#input-password').type("demo")
-// cy.get('[class="btn btn-primary"]').click()
-
-// cy.get('[class="btn-close"]').click()
-
-// cy.get('#menu-customer>a').click()
-
-// cy.get('#menu-customer>ul>li:first-child>a').click()
-// })
+cy.get('[class="btn-close"]').click()
+cy.get('#menu-customer>a').click()
+cy.get('#menu-customer>ul>li:first-child>a').click()
+ })
 it('Check Number Row and Columns', () => {
-  cy.visit('https://demo.opencart.com/admin/index.php')
+ 
 
     cy.get('[class="table table-bordered table-hover"]>tbody>tr').should('have.length','10')
     cy.get("[class='table table-bordered table-hover']>thead>tr>td").should('have.length','7')
@@ -45,18 +40,56 @@ cy.get('[class="table table-bordered table-hover"]>tbody>tr').each(($row,index,$
 })
 
 })
-      it('Pagination', () => {
+      it('Pagination1', () => {
 
-    cy.get('col-sm-6 text-end').then((e) => {
+      cy.get('[class="col-sm-6 text-end"]').then((e) => {
 
       let myTest = e.text()  //Showing 1 to 10 of 16313 (1632 Pages)
-    let   tatalPages= myTest.substring(myTest.indexOf("("+1),myTest.indexOf("P"-1))
+      cy.log("my test" + myTest)
+    
+      let   tatalPages= myTest.substring(myTest.indexOf("(")+1 , myTest.indexOf("P")-1)
 
-    cy.log('totalPages ==>'+ tatalPages)
+      cy.log('totalPages ==>>'+ tatalPages)
 
-      
+})
 
- })});
+
+});
+
+it.only('Pagination2', () => {
+
+    let totalNumPages =15;
+
+ 
+
+    for (let i = 1 ; i <=totalNumPages ;i++) {
+        cy.log("activePage  "+i)
+
+        cy.get('[class="table table-bordered table-hover"]>tbody>tr').each(($row,index,$rows)=>{
+
+            cy.wrap($row).within(()=>{
+                cy.get("td:nth-child(3)").then((e)=>{
+                   cy.log(e.text())
+                })
+       
+            })
+       
+       
+       
+               })
+
+        if(totalNumPages>1){
+             cy.get('[class="pagination"]>li:nth-child('+i+')').click()
+
+    }
+
+
+
+
+
+    }
+    
+});
 
 
 })
