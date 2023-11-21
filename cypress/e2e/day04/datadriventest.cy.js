2b520366adc2294208a191d20839ea63cb5f9957
nuri Kara <nurikara@yahoo.com>
2023-11-21 23:14:09 +0300

    dasda

--- cypress/e2e/day04/datadriventest.cy.js
+++ cypress/e2e/day04/datadriventest.cy.js
@@ -1,17 +1,19 @@
 describe("myTestSuit", () => {
   it("DataDrivenTest", () => {
+    
     cy.fixture("orangehrm2").then((data) => {
       cy.visit("https://opensource-demo.orangehrmlive.com/");
       data.forEach((userdata) => {
-        cy.get('[placeholder="Username"]').type(userdata.username);
-        cy.get('[placeholder="Password"]').type(userdata.password);
+       
+        cy.get('[name="username"]').type(userdata.username);
+        cy.get('[name="password"]').type(userdata.password);
 
         cy.get('[type="submit"]').click();
 
-        if ((userdata.username == "Admin") &(userdata.password == "admin123")) {
+        if ((userdata.username == "Admin") &&(userdata.password == "admin123")) {
           
-          cy.get('[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]').contains(userdata.expected)
-          cy.get('.oxd-userdropdown-tab > .oxd-icon').click()  
+          cy.get('[class="oxd-topbar-header-breadcrumb"]').should("have.text",userdata.expected)
+          cy.get('[class="oxd-icon bi-caret-down-fill oxd-userdropdown-icon"]').click()  
           cy.get(':nth-child(4) > .oxd-userdropdown-link').click()      
         }else{
           cy.get('[class="oxd-alert-content oxd-alert-content--error"]').contains(
